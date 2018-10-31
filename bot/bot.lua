51c55add4e393f633432401d1fbc8e69e79efc2d
saman9074 <saman9074@yahoo.com>
2018-10-31 09:13:05 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -1,6 +1,6 @@
 package.path = package.path..';.luarocks/share/lua/5.2/?.lua;.luarocks/share/lua/5.2/?/init.lua'
 package.cpath = package.cpath..';.luarocks/lib/lua/5.2/?.so'
-bot_token = "Token" --Put You Token Here
+bot_token = "697376469:AAHQZJaHgENZN67uNHXqf31tRQARYvQxuME" --Put You Token Here
 send_api = "https://api.telegram.org/bot"..bot_token
 sudo_id = 122630959
 require('./bot/methods')
